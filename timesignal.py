4cf8e81c516e4d0cea7b12be6daac4988462848a
shimoe <shimoewithranobe@gmail.com>
2020-05-21 01:43:25 +0900

    comment out print()

--- timesignal.py
+++ timesignal.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3
-#coding: UTF-8
+# coding: UTF-8
 
 import re
 import config
@@ -9,34 +9,34 @@ import datetime
 
 class MyStreamer(TwythonStreamer):
     def on_success(self, data):
-        if 'text' in data and 'user' != 'SW_Timesignal':
-            print(data['text'])
+        dt_now = datetime.datetime.now()
+        if 'text' in data and data['user']['screen_name'] != 'SW_Timesignal':
+            # print(data['text'])
             if len(re.findall('[0-9]+:[0-9]+', str(data['text']))):
                 if len(re.findall('[0-9]+\/[0-9]+\s+[0-9]+:[0-9]+', str(data['text']))):
                     time = re.findall(
                         '[0-9]+\/[0-9]+\s+[0-9]+:[0-9]+', str(data['text']))
-                    print(time[0])
+                    # print(time[0])
                 else:
-                    dt_now = datetime.datetime.now()
                     time = re.findall(
                         '[0-9]+:[0-9]+', str(data['text']))
                     time[0] = str(dt_now.month) + '/' + \
                         str(dt_now.day) + ' ' + str(time[0])
-                    print(time[0])
+                    # print(time[0])
             elif len(re.findall('[0-9]+時[0-9]+分', str(data['text']))):
                 if len(re.findall('[0-9]+月[0-9]+日*[0-9]+時[0-9]+分', str(data['text']))):
                     time = re.findall(
                         '[0-9]+月[0-9]+日*[0-9]+時[0-9]+分', str(data['text']))
-                    print(time[0])
+                    # print(time[0])
                 else:
                     time = re.findall('[0-9]+時[0-9]+分', str(data['text']))
                     time[0] = str(dt_now.month) + '月' + \
                         str(dt_now.day) + '日 ' + str(time[0])
-                    print(time[0])
+                    # print(time[0])
             username = data['user']['screen_name']
-            tweet = ("【星翼時報速報】\n %s \n from @%s \n#星翼\n#星翼時報" %
+            tweet = ("【星翼時報速報】\n %s \n from @%s \n#星翼時報" %
                      (time[0], username))
-            print(tweet)
+            # print(tweet)
             twitter.update_status(status=tweet)
 
     def on_error(self, status_code, data):
