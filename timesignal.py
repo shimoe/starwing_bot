2acbebac4a27c69f03dd99b51669de063608b5a3
shimoe <shimoewithranobe@gmail.com>
2020-05-28 01:58:28 +0900

    multi timesignal

--- timesignal.py
+++ timesignal.py
@@ -30,40 +30,49 @@ class MyStreamer(TwythonStreamer):
             tweet = []
             time = []
 
-            if len(re.findall('[0-9]+\/[0-9]+\s+[0-9]+:[0-9]+', data['text'])) or len(re.findall(
-                    '[0-9]+月[0-9]+日*[0-9]+時[0-9]+分', data['text'])):
-                print('date exist')
-                time.append(re.findall(
-                    '[0-9]+\/[0-9]+\s+[0-9]+:[0-9]+', data['text']))
-                time.append(re.findall(
-                    '[0-9]+月[0-9]+日*[0-9]+時[0-9]+分', data['text']))
-            else:
-                print('date no exist')
-                time.append(re.findall('[0-9]+:[0-9]+', data['text']))
-                time.append(re.findall('[0-9]+時[0-9]+分', data['text']))
+            # if len(re.findall('[0-9]+\/[0-9]+\s+[0-9]+:[0-9]+', data['text'])) or len(re.findall('[0-9]+月[0-9]+日*[0-9]+時[0-9]+分', data['text'])):
+            tweet_text = data['text'].splitlines()
+            print(tweet_text)
+            for line in tweet_text:
+                if len(re.findall('[0-9]+:[0-9]+', line)) > 0:
+                    if len(re.findall('[0-9]+\/[0-9]+\s+[0-9]+:[0-9]+', line)):
+                        time.extend(re.findall(
+                            '[0-9]+\/[0-9]+\s+[0-9]+:[0-9]+', line))
+                    else:
+                        time.extend(re.findall('[0-9]+:[0-9]+', line))
+                elif len(re.findall('[0-9]+時[0-9]+分', line)) > 0:
+                    if len(re.findall('[0-9]+月[0-9]+日.[0-9]+時[0-9]+分', line)):
+                        time.extend(re.findall(
+                            '[0-9]+月[0-9]+日.[0-9]+時[0-9]+分', line))
+                    else:
+                        time.extend(re.findall('[0-9]+時[0-9]+分', line))
+                else:
+                    print('no time in line')
 
             print(time)
             print('*******************')
 
-            if len(time):
-                for index, item in enumerate(time):
-                    if not len(re.findall('\/', str(item))):
-                        time[index] = (str(dt_now.month) + '/' +
-                                       str(dt_now.day) + ' ' + str(item))
-                    elif not len(re.findall('日', str(item))):
-                        time[index] = (str(dt_now.month) + '月' +
-                                       str(dt_now.day) + '日' + ' ' + str(item))
-                    # time[0] = str(dt_now.month) + '月' + \
-                    #    str(dt_now.day) + '日 ' + str(time[0])
-                print(time)
+            # if len(time):
+            #    for index, item in enumerate(time):
+            #        print('*******************')
+            #        if len(re.findall('\/', str(item))) > 0:
+            #            time[index] = (str(dt_now.month) + '/' +
+            #                           str(dt_now.day) + ' ' + str(item))
+            #            print(time[index])
+            #        elif len(re.findall('日', str(item))) > 0:
+            #            time[index] = (str(dt_now.month) + '月' +
+            #                           str(dt_now.day) + '日' + ' ' + str(item))
+            #            print(time[index])
+            #        else:
+            #            print(time[index])
+            #        # time[0] = str(dt_now.month) + '月' + \
+            #        #    str(dt_now.day) + '日 ' + str(time[0])
+            #    print(time)
 
-                tweet = ("【星翼時報速報】\n %s \n from @%s \n#星翼\n#星翼時報" %
-                         (time[0], username))
-                print(tweet)
-                # twitter.update_status(status=tweet)
-            else:
-                print('Time is not including.')
-                print(data['text'])
+            tweet = ("【星翼時報速報】\n %s \n from @%s \n#星翼\n#星翼時報" %
+                     (time[0], username))
+            print(tweet)
+            # twitter.update_status(status=tweet)
         else:
             print('unkwon error')
             print(data, sep='\n', end='------------------------------',
@@ -74,6 +83,7 @@ class MyStreamer(TwythonStreamer):
         # self.disconnect()
 
 
+print('awakening...')
 while True:
     twitter = Twython(config.TW_CONSUMER_KEY, config.TW_CONSUMER_SECRET,
                       config.TW_TOKEN, config.TW_TOKEN_SECRET)
