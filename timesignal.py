19890def48191869df05cf0ef27a9b380aa26a69
shimoe <shimoewithranobe@gmail.com>
2020-06-01 03:10:39 +0900

    debug

--- timesignal.py
+++ timesignal.py
@@ -12,18 +12,21 @@ import time
 class MyStreamer(TwythonStreamer):
     def on_success(self, data):
         print('---------------------------------')
-        time.sleep(5)
+        time.sleep(3)
         if 'retweeted_status' in data:
             print('Retweeted tweet')
         elif data['user']['screen_name'] == 'SW_Timesignal':
             print('self tweet')
         else:
+            print('target tweet')
             self.parse_tweet(data)
 
     def on_error(self, status_code, data):
         print(status_code)
 
     def serch_time_inline(self, text):
+        print(text)
+        print('*****************')
         dt_now = datetime.datetime.now()
         timesignal = []
         tweet_text = text.splitlines()
@@ -39,8 +42,30 @@ class MyStreamer(TwythonStreamer):
                         item = str(dt_now.month) + '/' + \
                             str(dt_now.day) + ' ' + i
                         timesignal.append(item)
-            elif len(re.findall('[0-9]+分', line)) > 0:
-                if len(re.findall('[0-9]+時[0-9]+分', line)) > 0:
+            elif len(re.findall('[0-9]+分', line)) > 0 or len(re.findall('[0-9]+時', line)) > 0:
+                # print('jap')
+                if len(re.findall('[0-9]+分', line)) > 0 and len(re.findall('[0-9]+時', line)) < 1:
+                    #print('no hour')
+                    nohour = re.findall('[0-9]+分', line)
+                    for i in nohour:
+                        time_value = re.findall('[0-9]+', i)
+                        if int(dt_now.minute) < int(time_value[0]):
+                            item = str(dt_now.month) + '月' + str(dt_now.day) + \
+                                '日' + ' ' + str(dt_now.hour) + '時' + i
+                            timesignal.append(item)
+                        else:
+                            item = str(dt_now.month) + '月' + str(dt_now.day) + \
+                                '日' + ' ' + str(int(dt_now.hour) + 1) + '時' + i
+                            timesignal.append(item)
+                elif len(re.findall('[0-9]+分', line)) < 1 and len(re.findall('[0-9]+時', line)) > 0:
+                    #print('no minute')
+                    nominute = re.findall('[0-9]+時', line)
+                    for i in nominute:
+                        item = str(dt_now.month) + '月' + str(dt_now.day) + \
+                            '日' + ' ' + i + '00分'
+                        timesignal.append(item)
+                elif len(re.findall('[0-9]+時[0-9]+分', line)) > 0:
+                    #print('time in line')
                     if len(re.findall('[0-9]+月[0-9]+日\s[0-9]+時[0-9]+分', line)):
                         timesignal.extend(re.findall(
                             '[0-9]+月[0-9]+日\s[0-9]+時[0-9]+分', line))
@@ -50,12 +75,6 @@ class MyStreamer(TwythonStreamer):
                             item = str(dt_now.month) + '月' + str(dt_now.day) + \
                                 '日' + ' ' + i
                             timesignal.append(item)
-                else:
-                    nohour = re.findall('[0-9]+分', line)
-                    for i in nohour:
-                        item = str(dt_now.month) + '月' + str(dt_now.day) + \
-                            '日' + ' ' + str(dt_now.hour) + i
-                        timesignal.append(item)
             else:
                 print('no time in line')
 
@@ -71,7 +90,7 @@ class MyStreamer(TwythonStreamer):
             if 'quoted_status' in data:
                 print('Quoted tweet')
                 base_text = data['text'].splitlines()
-                quoted_text = data['quoted_status']['text'].splitlines
+                quoted_text = data['quoted_status']['text'].splitlines()
 
                 for base in base_text:
                     tag_in_base = True if len(
@@ -81,12 +100,16 @@ class MyStreamer(TwythonStreamer):
                         re.findall('#星翼時報', quote)) > 0 else False
 
                 if tag_in_base == True and tag_in_quoted == False:
-                    timesignal = self.serch_time_inline(data['text'])
-                elif tag_in_base == False and tag_in_quoted == True:
+                    print('tag in base')
                     timesignal = self.serch_time_inline(
                         data['quoted_status']['text'])
+                elif tag_in_base == False and tag_in_quoted == True:
+                    print('tag in quote')
+                    timesignal = self.serch_time_inline(
+                        data['text'])
                 else:
-                    print('hash tag in both')
+                    timesignal = self.serch_time_inline(data['text'])
+                    print('tag in both')
             else:
                 timesignal = self.serch_time_inline(data['text'])
 
